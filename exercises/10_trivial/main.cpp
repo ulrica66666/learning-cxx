0da424434dfb371aa2b913b29d4ceb756b878fd3
ulrica66666 <2672801043@qq.com>
2025-01-22 18:39:16 +0000

    exercise15 passed

--- exercises/10_trivial/main.cpp
+++ exercises/10_trivial/main.cpp
@@ -8,18 +8,42 @@ struct FibonacciCache {
 };
 
 // TODO: 实现正确的缓存优化斐波那契计算
+// static unsigned long long fibonacci(FibonacciCache &cache, int i) {
+//     int j=cache.cached;
+//     for (; j<=i; ++j) {
+//         cache.cache[j] = cache.cache[j - 1] + cache.cache[j- 2];
+//     }
+//     return cache.cache[i];
+// }
+// 计算斐波那契数列，利用缓存来加速计算
 static unsigned long long fibonacci(FibonacciCache &cache, int i) {
-    for (; false; ++cached) {
-        cache[cached] = cache[cached - 1] + cache[cached - 2];
+    if (i <= 1) {
+        return i;  // 斐波那契数列的前两个项是 0 和 1
     }
-    return cache.cache[i];
+
+    // 如果缓存中没有计算过第 i 项，则进行计算并缓存
+    if (cache.cached <= i) {
+        for (int j = cache.cached; j <= i; ++j) {
+            if (j == 0) {
+                cache.cache[j] = 0;  // F(0) = 0
+            } else if (j == 1) {
+                cache.cache[j] = 1;  // F(1) = 1
+            } else {
+                cache.cache[j] = cache.cache[j - 1] + cache.cache[j - 2];  // F(n) = F(n-1) + F(n-2)
+            }
+        }
+        cache.cached = i + 1;  // 更新已缓存的项数
+    }
+    
+    return cache.cache[i];  // 返回第 i 项的斐波那契数
 }
 
 int main(int argc, char **argv) {
     // TODO: 初始化缓存结构体，使计算正确
     // NOTICE: C/C++ 中，读取未初始化的变量（包括结构体变量）是未定义行为
     // READ: 初始化的各种写法 <https://zh.cppreference.com/w/cpp/language/initialization>
-    FibonacciCache fib;
+    FibonacciCache fib={{0},0};
+
     ASSERT(fibonacci(fib, 10) == 55, "fibonacci(10) should be 55");
     std::cout << "fibonacci(10) = " << fibonacci(fib, 10) << std::endl;
     return 0;
--- exercises/11_method/main.cpp
+++ exercises/11_method/main.cpp
@@ -1,18 +1,43 @@
 #include "../exercise.h"
 
-struct Fibonacci {
-    unsigned long long cache[128];
-    int cached;
+// struct Fibonacci {
+//     unsigned long long cache[128];
+//     int cached;
 
-    // TODO: 实现正确的缓存优化斐波那契计算
+//     // TODO: 实现正确的缓存优化斐波那契计算
+//     unsigned long long get(int i) {
+//         for (; false; ++cached) {
+//             cache[cached] = cache[cached - 1] + cache[cached - 2];
+//         }
+//         return cache[i];
+//     }
+// };
+struct Fibonacci {
+    unsigned long long cache[128];  // 用于缓存最多 128 项斐波那契数列的值
+    int cached;                     // 表示当前已缓存的最大斐波那契项的数量
+
+    // 构造函数：初始化缓存结构体
+    Fibonacci() : cached(0) {
+        cache[0] = 0;  // F(0) = 0
+        cache[1] = 1;  // F(1) = 1
+        cached = 2;    // 初始缓存了前两项
+    }
+
+    // 获取第 i 项斐波那契数，使用缓存优化
     unsigned long long get(int i) {
-        for (; false; ++cached) {
-            cache[cached] = cache[cached - 1] + cache[cached - 2];
+        if (i < cached) {
+            return cache[i];  // 如果缓存中已有值，直接返回
         }
-        return cache[i];
+
+        // 计算并缓存缺失的斐波那契数
+        for (int j = cached; j <= i; ++j) {
+            cache[j] = cache[j - 1] + cache[j - 2];  // F(n) = F(n-1) + F(n-2)
+        }
+
+        cached = i + 1;  // 更新已缓存的最大项数
+        return cache[i];  // 返回第 i 项
     }
 };
-
 int main(int argc, char **argv) {
     // TODO: 初始化缓存结构体，使计算正确
     Fibonacci fib;
